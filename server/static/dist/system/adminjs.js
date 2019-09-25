0d4ef22fead9eb5ead2860a9974aded89ed90d01
farhapartex <farhapartex@gmail.com>
2019-09-25 10:44:33 +0600

    vue cdn added to admin app

--- server/static/dist/system/adminjs.js
+++ server/static/dist/system/adminjs.js
@@ -1,18 +1,44 @@
-$(document).ready(function () {
-    $("#psearch").keydown(function () {
-        var host = $(location).attr('host');
-        var purl = host + "/api/v1/public/posts";
-        $.ajax({
-            type: "get",
-            url: purl,
-            dataType: "json",
-            success: function (result) {
-                console.log(result);
-            },
-            error: function (error) {
-                console.log("data pay na");
-            }
-        });
+// $(document).ready(function () {
+//     $("#psearch").keydown(function () {
+//         var host = $(location).attr('host');
+//         var purl = host + "/api/v1/public/posts";
+//         $.ajax({
+//             type: "get",
+//             url: purl,
+//             dataType: "json",
+//             success: function (result) {
+//                 console.log(result);
+//             },
+//             error: function (error) {
+//                 console.log("data pay na");
+//             }
+//         });
 
-    });
-});
+//     });
+// });
+http://127.0.0.1:8000/cms/posts/
+var app5 = new Vue({
+    el: '#cms',
+    data: {
+        host: location.host,
+        public_api: "/api/v1/public/"
+    },
+    methods: {
+        getPostList() {
+            axios
+                .get('http://' + this.host + this.public_api + "posts/")
+                .then(response => (
+                    console.log(response.data)
+                ))
+                .catch(error => console.log(error))
+        },
+        postSearch: function () {
+            console.log("Working..");
+            console.log(this.host);
+
+        }
+    },
+    mounted() {
+        this.getPostList();
+    },
+})
