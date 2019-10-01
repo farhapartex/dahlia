23358f9fda46e91fc1d5c727608fd00778696419
farhapartex <farhapartex@gmail.com>
2019-10-01 12:06:57 +0600

    post_url added

--- server/blog/models.py
+++ server/blog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
+
 # Create your models here.
 
-'''
+"""
 Models:
 Post, Comment, React, Tag
-'''
+"""
+
 
 class Category(models.Model):
     name = models.CharField(_("Category"), max_length=150)
@@ -19,22 +22,33 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Tag(models.Model):
-    name =models.CharField(_("Tag"), max_length=50)
+    name = models.CharField(_("Tag"), max_length=50)
     created_at = models.DateTimeField(_("Created At"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated At"), auto_now=True)
 
     def __str__(self):
         return self.name
 
-PUBLISH_CHOICE = ((True,"Publish"),(False,"Not Publish"))
+
+PUBLISH_CHOICE = ((True, "Publish"), (False, "Not Publish"))
+
+
 class Post(models.Model):
     title = models.CharField(_("Title"), max_length=150)
     subtitle = models.CharField(_("Sub Title"), max_length=250, blank=True, null=True)
     body = models.TextField(_("Post"))
-    category = models.ForeignKey(Category, verbose_name=_("Category"), related_name="posts", on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category,
+        verbose_name=_("Category"),
+        related_name="posts",
+        on_delete=models.CASCADE,
+    )
     tags = models.ManyToManyField(Tag, verbose_name=_("Tags"))
-    published = models.BooleanField(_("Published"), choices=PUBLISH_CHOICE, default=False)
+    published = models.BooleanField(
+        _("Published"), choices=PUBLISH_CHOICE, default=False
+    )
 
     created_at = models.DateTimeField(_("Created At"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated At"), auto_now=True)
@@ -42,22 +56,39 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-REACT_TYPES = ((1,"LIKE"),(2,"DISLIKE"),(3,"CLAP"),(4,"LOVE"))
+    def get_absolute_url(self):
+        return "/api/v1/public/posts/{0}/".format(self.id)
+
+
+REACT_TYPES = ((1, "LIKE"), (2, "DISLIKE"), (3, "CLAP"), (4, "LOVE"))
+
+
 class React(models.Model):
-    post = models.ForeignKey(Post, verbose_name=_("Post"),related_name="reacts", on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, verbose_name=_("Post"), related_name="reacts", on_delete=models.CASCADE
+    )
     amount = models.PositiveIntegerField(_("Amount"))
     type = models.SmallIntegerField(_("Type"), choices=REACT_TYPES, default=1)
 
     def __str__(self):
-        return f'{self.post} {self.amount}'
+        return f"{self.post} {self.amount}"
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, verbose_name=_("Post"),related_name="comments", on_delete=models.CASCADE)
-    parent = models.ForeignKey("self", verbose_name=_("Parent"),related_name="children", on_delete=models.SET_NULL, blank=True, null=True)
+    post = models.ForeignKey(
+        Post, verbose_name=_("Post"), related_name="comments", on_delete=models.CASCADE
+    )
+    parent = models.ForeignKey(
+        "self",
+        verbose_name=_("Parent"),
+        related_name="children",
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+    )
     body = models.TextField(_("Comment Text"))
     created_at = models.DateTimeField(_("Created At"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated At"), auto_now=True)
 
     def __str__(self):
-        return f'{self.post} {self.body[:30]}' 
+        return f"{self.post} {self.body[:30]}"
--- server/blog/serializers.py
+++ server/blog/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import *
 
+
 class PublicCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ("name",)
+
 
 class PublicTagSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,46 +16,67 @@ class PublicTagSerializer(serializers.ModelSerializer):
 
 class FlatPostSerializer(serializers.ModelSerializer):
     category = PublicCategorySerializer()
+    post_url = serializers.SerializerMethodField()
     tags = PublicTagSerializer(many=True)
+
     class Meta:
         model = Post
-        fields = ("id","title","category","tags","created_at")
+        fields = ("id", "post_url", "title", "category", "tags", "created_at")
+
+    def get_post_url(self, obj):
+        return obj.get_absolute_url()
+
 
 class ReactSerializer(serializers.ModelSerializer):
     class Meta:
         model = React
-        fields = ("id","type","amount")
+        fields = ("id", "type", "amount")
+
 
 class ChildCommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ("id","parent","body","created_at","updated_at")
+        fields = ("id", "parent", "body", "created_at", "updated_at")
+
 
 class RecursiveField(serializers.Serializer):
     def to_representation(self, value):
-        serializer = self.parent.parent.__class__(
-            value,
-            context=self.context)
+        serializer = self.parent.parent.__class__(value, context=self.context)
         return serializer.data
+
 
 class CommentSerializer(serializers.ModelSerializer):
     children = RecursiveField(many=True)
+
     class Meta:
         model = Comment
-        fields = ("id","parent","children","body","created_at","updated_at")
+        fields = ("id", "parent", "children", "body", "created_at", "updated_at")
+
 
 class PublicPostSerializer(serializers.ModelSerializer):
     category = PublicCategorySerializer()
     tags = PublicTagSerializer(many=True)
     reacts = ReactSerializer(many=True)
     # comments = CommentSerializer(many=True)
-    comments= serializers.SerializerMethodField()
+    comments = serializers.SerializerMethodField()
 
-    def get_comments(self,obj):
-         post_comment = Comment.objects.filter(post = obj.id, parent = None)
-         serializer = CommentSerializer(post_comment,many=True)
-         return serializer.data
+    def get_comments(self, obj):
+        post_comment = Comment.objects.filter(post=obj.id, parent=None)
+        serializer = CommentSerializer(post_comment, many=True)
+        return serializer.data
 
     class Meta:
         model = Post
-        fields = ("id","title","subtitle","body","category","tags","reacts","comments","published","created_at")
+        fields = (
+            "id",
+            "title",
+            "subtitle",
+            "body",
+            "category",
+            "tags",
+            "reacts",
+            "comments",
+            "published",
+            "created_at",
+        )
+
